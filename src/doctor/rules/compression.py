390f53ec1e5ebdc746faa3381bcbad6713edc354
Mats Kindahl <mats@timescale.com>
2023-08-17 22:05:08 +0200

    feat: add rule to check pointless segment-by column
    If the statistics say that the number of distinct values in a
    segment-by column is 1, it is pointless to use as a segment-by column.

--- src/doctor/rules/compression.py
+++ src/doctor/rules/compression.py
@@ -17,3 +17,20 @@ SELECT format('%I.%I', schema_name, table_name)::regclass AS relation, s.attname
 def linear_segmentby(cursor):
     """Column '{attname}' in hypertable '{relation}' as segment-by column is probably not a good choice since the number of values seems to grow with the number of rows of the table."""
     cursor.execute(LINEAR_QUERY)
+
+POINTLESS_QUERY = """
+SELECT format('%I.%I', schema_name, table_name)::regclass AS relation, s.attname
+  FROM _timescaledb_catalog.hypertable_compression c
+  JOIN _timescaledb_catalog.hypertable h ON c.hypertable_id = h.id
+  JOIN pg_stats s
+    ON s.attname = c.attname
+   AND s.schemaname = h.schema_name
+   AND s.tablename = h.table_name
+ WHERE segmentby_column_index IS NOT NULL AND n_distinct = 1;
+"""
+
+@rule(__name__)
+def pointless_segmentby(cursor):
+    """Column '{attname}' in hypertable '{relation}' as segment-by column is pointless since it contains a single value."""
+    cursor.execute(POINTLESS_QUERY)
+    
