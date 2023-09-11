1919aace7cb083ea7a86c3e63b18c3f14b48986a
Mats Kindahl <mats@timescale.com>
2023-09-11 16:04:33 +0200

    docs: add license text
    License text is added to all major files and also to the README.

--- src/doctor/rules/__init__.py
+++ src/doctor/rules/__init__.py
@@ -1,9 +1,22 @@
+# Copyright 2023 Timescale, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Rules to check for a database.
 
 This is the package containing all rules. To add an additional rule,
 add a separate file to this directory and define any rules you deem
 necessary.
-
 """
 
 import glob
